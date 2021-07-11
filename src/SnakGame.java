a9298fb475b8bedf3b2aaeffdc33283a61ff54c8
NikolayIT <tkachenni@mail.ru>
2021-07-11 15:20:20 +0300

    First commit

---
+++ src/SnakGame.java
@@ -0,0 +1,15 @@
+package src;
+
+public class SnakGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Snak Game");
+		
+		GameFrame frame = new GameFrame();
+		
+		
+	}
+
+}
